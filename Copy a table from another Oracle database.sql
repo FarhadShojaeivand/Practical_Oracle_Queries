acf7f0e4a63dbc0ab8a56bdedbfcae2361c48812
FarhadShojaeivand <106688862+FarhadShojaeivand@users.noreply.github.com>
2024-02-04 13:50:26 +0330

    Update Copy a table from another Oracle database.sql

--- Copy a table from another Oracle database.sql
+++ Copy a table from another Oracle database.sql
@@ -1,3 +1,2 @@
 -- using Oracle SQL Developer
-copy from <user_source>@<IP_source>:<port>/<servicename_source> to <user_target>@<IP_target>:<port>/<servicename_target> 
-create <table_name_target> using select * from <table_name_source>
+copy from <user_source>/<password>@<IP_source>:<port>/<servicename_source> to <user_target>/<password>@<IP_target>:<port>/<servicename_target> create <table_name_target> using select * from <table_name_source>
